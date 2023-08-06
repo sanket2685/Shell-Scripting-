da524210c076d138b235e2b9ca34dd2883e6b88a
sanket2685 <sanketsonar63@gmail.com>
2023-08-06 13:04:01 +0530

    new code added

--- arrays.sh
+++ arrays.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 array=(1 2 3 4 hello world)
 echo "value of 3rd index ${array[4]}"
+
+
+
+#!/bin/bash
+declare -A array
+array=( [name]=sanket [age]=25 [city]=pune )
+echo "name is ${array[name]}"
+echo "age is ${array[age]}"
+echo "city is ${array[city]}"
